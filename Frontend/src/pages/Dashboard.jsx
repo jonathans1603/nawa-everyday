f9c18fe385587544d0246ffcd76bfa8983b56cbc
jonathans1603 <yonathans1603@gmail.com>
2026-05-11 19:19:04 +0700

    a

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -12,7 +12,6 @@ export default function Dashboard({ onNavigate }) {
         <Hero />
         <Specialities />
         <Facility />
-        <Facility />
       </main>
       <Footer />
     </div>
