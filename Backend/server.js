a3b61bcd1009b2d6d400821530c04afd1e33aac8
jonathans1603 <yonathans1603@gmail.com>
2026-05-20 17:08:00 +0700

    20

--- Backend/server.js
+++ Backend/server.js
@@ -32,4 +32,5 @@ app.use('/api', aboutRoutes);            // ← tambah
 
 app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
